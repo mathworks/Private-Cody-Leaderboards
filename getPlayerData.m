852efd11138b5ed3f09b9332d0db3f7c92a6c242
Jason Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2021-01-07 20:18:44 -0600

    Update getPlayerData.m
    Fixed the problem when there is no activity on a Cody players page.

--- getPlayerData.m
+++ getPlayerData.m
@@ -13,11 +13,32 @@ profTarget = 'matlabcentral/profiles/(.*?)\.(jpg|jpeg|png)';
 tk = regexpi(html,profTarget,'tokens','once');
 img = sprintf('https://www.mathworks.com/matlabcentral/profiles/%s.%s',tk{1},tk{2});
 
-% extract the score, badge, and Cody name from a hyperlink title
-target = '<a title="Score: (\d+), Badges: (\d+)".*?href="/matlabcentral/cody/players/.*?">(.*?)</a>';
-tk = regexp(html,target,'tokens','once');
-name = tk{3};
-score = str2double(tk{1});
-badgeCount = str2double(tk{2});
 
+% extract the score, badge, and Cody name from a hyperlink title
+% extracting this way is the easy way. However, it only works if the user has played Cody and has activity on their
+% profile. Is it possible to have no activity but have a score? Not sure.
+    target = '<a title="Score: (\d+), Badges: (\d+)".*?href="/matlabcentral/cody/players/.*?">(.*?)</a>';
+    tk = regexp(html,target,'tokens','once');
+    if ~isempty(tk)
+        name = tk{3};
+        score = str2double(tk{1});
+        badgeCount = str2double(tk{2});
+    else
+        % Use a more difficult way to extract the name, score, and badgeCount.
+        % This can handle no activity on the Cody player's page.
+        
+        % This is the only pattern that occurs for the name
+        namePattern = '<h1 class="add_margin_10">(.+?)</h1>';
+        name = regexp(html,namePattern,'tokens','once');
+        name = name{1};
+        
+        % White space \s can break up this set of html tags so we try to guard against
+        % this by using the \s*
+        scorePattern = '>\s*(\d*)\s*</h\d>\s*Score\s*</div>';
+        score = regexp(html,scorePattern,'tokens','once');
+        score = str2double(score{1});
+        badgePattern = '>\s*(\d+)\s*</h\d>\s*Badges\s*</div>';
+       badgeCount = regexp(html,badgePattern,'tokens','once');
+       badgeCount = str2double(badgeCount{1});
+    end
 end
