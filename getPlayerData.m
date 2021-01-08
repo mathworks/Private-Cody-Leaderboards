31196b31d7732d25c2d5f62748501babeae0dd72
Jason Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2021-01-07 18:57:41 -0600

    Update getPlayerData.m
    I fixed a bug I introduced related to extracting the image name.

--- getPlayerData.m
+++ getPlayerData.m
@@ -9,9 +9,9 @@ url = sprintf('https://www.mathworks.com/matlabcentral/cody/players/%d',id);
 html = webread(url);
 
 % extract the image url
-profTarget = 'matlabcentral/profiles/.*?\.(jpg|jpeg|png)';
+profTarget = 'matlabcentral/profiles/(.*?)\.(jpg|jpeg|png)';
 tk = regexpi(html,profTarget,'tokens','once');
-img = sprintf('https://www.mathworks.com/matlabcentral/profiles/%d.%s',id,tk{1});
+img = sprintf('https://www.mathworks.com/matlabcentral/profiles/%s.%s',tk{1},tk{2});
 
 % extract the score, badge, and Cody name from a hyperlink title
 target = '<a title="Score: (\d+), Badges: (\d+)".*?href="/matlabcentral/cody/players/.*?">(.*?)</a>';
