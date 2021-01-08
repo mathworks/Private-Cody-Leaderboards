48dcff1e7bcce0e4809f90880e68db32133b3a87
Jason Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2021-01-07 18:50:51 -0600

    Update getPlayerData.m
    The Cody player page has changed. The regular expressions needed an update.

--- getPlayerData.m
+++ getPlayerData.m
@@ -1,23 +1,23 @@
 function [name, url, img, score, badgeCount] = getPlayerData(id)
-    %Use screen-scraping to return information about a Cody player given Player ID
-    %   [name, url, img, score, badgeCount] = getPlayerData(id) 
-    
-    url = sprintf('https://www.mathworks.com/matlabcentral/cody/players/%d',id);
-    html = webread(url);
-    
-    profUrl = sprintf('https://www.mathworks.com/matlabcentral/cody/players/%d',id);
-    profHtml = webread(profUrl);
-    profTarget = 'matlabcentral/profiles/(.*?)\.(jpg|jpeg|png)';
-    tk = regexpi(profHtml,profTarget,'tokens','once');
-    
-    img = sprintf('https://www.mathworks.com/matlabcentral/profiles/%s.%s',tk{1},tk{2});
-    
-    target = '<a href="/matlabcentral/cody/players/.*?" title="Score: (\d+), Badges: (\d+)">(.*?)</a>';
-    tk = regexp(html,target,'tokens','once');
-    
-    
-    name = tk{3};
-    score = str2double(tk{1});
-    badgeCount = str2double(tk{2});
-    
+%Use screen-scraping to return information about a Cody player given Player ID
+%
+%   [name, url, img, score, badgeCount] = getPlayerData(id)
+%
+
+% Cody player's page
+url = sprintf('https://www.mathworks.com/matlabcentral/cody/players/%d',id);
+html = webread(url);
+
+% extract the image url
+profTarget = 'matlabcentral/profiles/.*?\.(jpg|jpeg|png)';
+tk = regexpi(html,profTarget,'tokens','once');
+img = sprintf('https://www.mathworks.com/matlabcentral/profiles/%d.%s',id,tk{1});
+
+% extract the score, badge, and Cody name from a hyperlink title
+target = '<a title="Score: (\d+), Badges: (\d+)".*?href="/matlabcentral/cody/players/.*?">(.*?)</a>';
+tk = regexp(html,target,'tokens','once');
+name = tk{3};
+score = str2double(tk{1});
+badgeCount = str2double(tk{2});
+
 end
