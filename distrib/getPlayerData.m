bba3f973d7b298fe3ee5d0e873f90d7317d7dbdb
gulley <gulley@mathworks.com>
2022-06-29 12:13:34 -0400

    fix score-scraping code

---
+++ distrib/getPlayerData.m
@@ -0,0 +1,23 @@
+function [name, url, img, score, badgeCount] = getPlayerData(id)
+    %Use screen-scraping to return information about a Cody player given Player ID
+    %   [name, url, img, score, badgeCount] = getPlayerData(id) 
+    
+    url = sprintf('https://www.mathworks.com/matlabcentral/cody/players/%d',id);
+    html = webread(url);
+    
+    profUrl = sprintf('https://www.mathworks.com/matlabcentral/cody/players/%d',id);
+    profHtml = webread(profUrl);
+    profTarget = 'matlabcentral/profiles/(.*?)\.(jpg|jpeg|png)';
+    tk = regexpi(profHtml,profTarget,'tokens','once');
+    
+    img = sprintf('https://www.mathworks.com/matlabcentral/profiles/%s.%s',tk{1},tk{2});
+    
+    target = '<a title="Score: (\d+), Badges: (\d+).*?>(.*?)</a>';
+    tk = regexp(html,target,'tokens','once');
+    
+    
+    name = tk{3};
+    score = str2double(tk{1});
+    badgeCount = str2double(tk{2});
+    
+end
