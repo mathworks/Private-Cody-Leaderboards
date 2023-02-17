c4c457ccc8689ceef36110b5d65f30b0b6942eed
gulley <gulley@mathworks.com>
2023-02-17 17:48:24 -0500

    Update smokeTest.m

--- test/smokeTest.m
+++ test/smokeTest.m
@@ -2,7 +2,10 @@
 
 %%
 
- playerId = 734801;
- [name, url, img, score, badgeCount] = getPlayerData(playerId)
+playerId = 734801;
+[name, url, img, score, badgeCount] = getPlayerData(playerId);
+assert(isequal(name,"Binbin Qi"))
+assert(isequal(url,'https://www.mathworks.com/matlabcentral/cody/players/734801'))
+assert(isequal(img,'https://www.mathworks.com/matlabcentral/profiles/734801.jpg'))
 
 %%
